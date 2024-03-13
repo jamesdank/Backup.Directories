3c78d77fb7f8d3d2e8b26b1bad1a37c017fed6d7
jamesdank <94645949+jamesdank@users.noreply.github.com>
2024-03-13 07:27:43 -0500

    Create backup.sh

---
+++ backup.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+# Destination for the backup files
+destination=""
+
+date="$(date +%Y-%m-%d_%H%M%S)"
+
+mkdir -p "$destination/backup-$date"
+
+# These are examples of directories to backup
+# The first part is the name of the folder
+# The second part is the directory
+# make sure there is no / in the beginning
+declare -A directories=(
+    ["home"]="home/"
+    ["etc"]="etc/"
+    ["var"]="var/"
+    );
+
+echo -e "Please wait Creating Backups\n"
+sleep 2
+
+for key in ${!directories[@]}
+do
+    tar -czf "$destination/backup-$date/${key}.tar.gz" -C / "${directories[${key}]}"
+
+    echo ${key} Backup Created.
+done
